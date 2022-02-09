6f2ac399945f2e830db786c63cda529a3fa68e87
wiscaksono <wwicaksono96@gmail.com>
2022-02-09 15:02:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        paragraph: "#565656",
+        blue: "#2639ED",
+      },
+      backgroundImage: {
+        hero: "url('/images/bghero.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
